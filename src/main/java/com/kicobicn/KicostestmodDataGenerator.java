643c9f3646dcc6166dfcc87f3e2226f470e49fda
kicobicn <1848667424@qq.com>
2024-08-18 10:17:03 +0800

    minecraft mod test

---
+++ src/main/java/com/kicobicn/KicostestmodDataGenerator.java
@@ -0,0 +1,11 @@
+package com.kicobicn;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class KicostestmodDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
